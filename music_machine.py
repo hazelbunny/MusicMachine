4962fe53911f779ea0754a23d6540ed439b2f90c
hazelbunny <hazelsayuricrompton@gmail.com>
2021-11-06 15:37:58 +0000

    Program now "works", so I moved the legacy code into a new branch and renamed some files.

--- music_machine.py
+++ music_machine.py
@@ -1,66 +1,148 @@
+from gpiozero import Button
+import time
 from psonic import *
 from threading import Thread
-set_server_parameter('127.0.0.1',4557,4559)
+from json import load as json_load
+from numpy import random
+import numpy
+from musthe import *
 
 
-#tick = Message()
 
-bpm = 140
+BEATS_PER_MINUTE=140
+KEY_SIGNATURE=Note('C')
+TIME_SIGNATURE_TOP=4
+TIME_SIGNATURE_TOP=4
 
-def in_thread(func):
-    def wrapper(*args,**kwargs):
-        print(type(kwargs))
-        _thread = threading.Thread(target=func,args=args)
-        key_args={}
-        for key in kwargs:
-            key_args[key]=kwargs[key]
-        _thread._kwargs=key_args
-        _thread.start()
+globals().update({"DEBUG":False})
 
+
+
+with open("templates.json") as file:
+    templates = json_load(file)
+    file.close()
+    
+#with open("instruments.json") as file:
+#    instruments = json_load(file)
+#    file.close()
+
+
+
+if DEBUG: print(templates)
+
+beat_length = 60/140
+bar_length = beat_length * TIME_SIGNATURE_TOP
+
+tracklist = []
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        Thread(target=fn, args=args, kwargs=kwargs).start()
     return wrapper
 
+def new_bar(role, strum_pattern):
+    bar=[]
+    print(strum_pattern)
+    role = "melody"
+    if role == "melody":
+        tie = False
+        for i in strum_pattern:
+            if random.randint(0,6) == 0: tie = True
+            if tie:
+                bar.append("tie")
+            elif i == "1":
+                bar.append(random.choice(chord(C4,MAJOR7)))
+            else:
+                bar.append(None)
+    return bar
+
 @in_thread
-def psn(pitch, **kwargs): #play a single note
-    nkwargs={}
-    print(type(kwargs))
-    if 'synth' in kwargs.keys():
-        use_synth(kwargs['synth'])
-    if 'repeat' not in kwargs.keys():
-        kwargs['repeat']=1
-        kwargs['sleep']=0
-    if 'pattern' not in kwargs.keys():
-        kwargs['pattern']="////"
-    for key in kwargs:
-        if key != 'synth' and key != 'sleep' and key != 'repeat' and key != 'pattern':
-            nkwargs[key] = kwargs[key]
-    for i in range(kwargs['repeat']):
-        for determine in kwargs['pattern']:
-            if determine == "/":
-                play(pitch,**nkwargs)
-            sleep(kwargs['sleep']/(len(kwargs['pattern'])/4))
+def delete_thread(to_go):
+    if DEBUG: print("deleting "+str(to_go))
+    time.sleep(bar_length)
+    del to_go
 
-def arpeggiate(notes, pattern, **kwargs):
-    note_num = 0
-    for determine in pattern:
-        if determine == "/":
-            note_num+=1
-            psn(notes[note_num%len(notes)])
-
-def str_to_array(input_str):
-    val = []
-    for i in range(len(input_str)):
-        print(i)
-        val.append(input_str[i])
+class Track:
+    def __init__(self):
+        global DEBUG
+        global templates
+        if DEBUG: print("new Track object created")
+        self.end=False
+        self.instrument=""
+        self.template=templates[str(random.randint(1,len(templates.keys())))]
         
-strum = "/ / /   // / // "
+        
+        self.shuffle()
+    def end_tasks(self):
+        self.end=True
+    def shuffle(self):
+        self.bars=[]
+        self.default_asdr={
+            "attack":0.1,
+            "decay":0,
+            "sustain_level":1,
+            "sustain":0.5,
+            "release":0.4
+            }
+        if DEBUG: print(str(self)+" is being shuffled")
+        print(self.template)
+        for bar in range(int(self.template["bars"])):
+            self.bars.append(new_bar(self.template["role"],random.choice(self.template["strum_patterns"])))
+        self.instrument=""
+        print(self.bars)
+    @threaded
+    def tick(self, global_bar):
+        if DEBUG: print("music sync from "+str(self))
+        bar = self.bars[global_bar%len(self.bars)]
+        position = 1
+        sleeptime=bar_length/len(bar)
+        for i in bar:
+            if i == "tie":
+                pass
+            elif type(i) == numpy.int32:
+                asdr=self.default_asdr.copy()
+                temppos=position
+                while temppos < len(bar) and bar[temppos]=="tie":
+                    asdr["release"]+=beat_length/(len(bar)/TIME_SIGNATURE_TOP)
+                    temppos+=1
+                play(i,**asdr)
+                print("note: "+str(i)+" with sustain: "+str(asdr["sustain"]))
+            elif type(i) == samples.Sample:
+                sample(i)
+            time.sleep(sleeptime)
+            position +=1
+        
 
 
-#mainloop()
-psn(70)
-sleep(2)
-psn(70,synth=PROPHET)
-mychord=chord(E3,MAJOR7)
-psn(mychord,synth=PLUCK,repeat=4,sleep=1,pattern=strum)
-arpeggiate(mychord, "/ / / / / / / /")
+
+blue_button = Button("GPIO21")
+black_button = Button("GPIO16")
+red_button = Button("GPIO20")
+brown_button = Button("GPIO26")
+
+def shuffle_most_recent():
+    if len(tracklist)>0:
+        tracklist[len(tracklist)-1].shuffle()
+
+def delete_most_recent():
+    if len(tracklist)>0:
+        delete_thread(tracklist.pop(len(tracklist)-1))
+
+def add_new_track():
+    tracklist.append(Track())
+
+def hello_world():
+    print("hello world")
+
+blue_button.when_pressed = add_new_track
+red_button.when_pressed = delete_most_recent
+black_button.when_pressed = shuffle_most_recent
+brown_button.when_pressed = hello_world
+
+
+bar = 1
 while True:
-    pass
+    for track in tracklist:
+        track.tick(bar)
+    time.sleep(bar_length)
+    bar +=1
